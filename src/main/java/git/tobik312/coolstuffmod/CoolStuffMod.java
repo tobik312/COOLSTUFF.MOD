d47b939259910f686c860515061c04ba5678dd6a
tobik312 <tobik-10@tlen.pl>
2018-03-08 14:50:45 +0100

    update

---
+++ src/main/java/git/tobik312/coolstuffmod/CoolStuffMod.java
@@ -0,0 +1,17 @@
+package git.tobik312.coolstuffmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(modid = CoolStuffMod.MODID, name = CoolStuffMod.NAME, version = CoolStuffMod.VERSION)
+public class CoolStuffMod {
+	
+	public static final String MODID = "coolstuffmod";
+    public static final String NAME = "Cool stuff mod";
+    public static final String VERSION = "0.1";
+
+}
