ab09a6120325d6659230e869e824224fbb77709e
tobik312 <tobik-10@tlen.pl>
2018-03-08 17:35:07 +0100

    cios

--- src/main/java/git/tobik312/coolstuffmod/CoolStuffMod.java
+++ src/main/java/git/tobik312/coolstuffmod/CoolStuffMod.java
@@ -2,19 +2,8 @@ package git.tobik312.coolstuffmod;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import org.apache.logging.log4j.Logger;
 
 
 @Mod(modid = CoolStuffMod.MODID, name = CoolStuffMod.NAME, version = CoolStuffMod.VERSION)
