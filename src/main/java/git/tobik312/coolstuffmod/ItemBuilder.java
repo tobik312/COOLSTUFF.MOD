83395768364950e568e6afd25916f72b2c42274e
tobik312 <tobik-10@tlen.pl>
2018-03-08 15:47:30 +0100

    test

--- src/main/java/git/tobik312/coolstuffmod/ItemBuilder.java
+++ src/main/java/git/tobik312/coolstuffmod/ItemBuilder.java
@@ -4,11 +4,12 @@ import net.minecraft.item.Item;
 
 public class ItemBuilder extends Item {
 	
-	public ItemBuilder(String name,int maxSize){
+	public ItemBuilder(String name,int maxSize,String textureName){
 		setCreativeTab(CoolStuffMod.creativeTab);
 		setUnlocalizedName(name);
 		setMaxStackSize(maxSize);
 		setRegistryName(name);
+		setTextureName(CoolStuffMod.MODID+":"+textureName+".png");
 	}
 
 }
