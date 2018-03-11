a6ec5d12f2ed72f8b52f0cf33a50e8368270e898
Xesar <fzakowicz@op.pl>
2018-03-11 12:56:05 +0100

    update
    ITS NOT WORKING
    SOMETHING IS WRONG
    DUNNO WHAT

--- src/main/java/git/tobik312/coolstuffmod/utils/Reference.java
+++ src/main/java/git/tobik312/coolstuffmod/utils/Reference.java
@@ -2,7 +2,7 @@ package git.tobik312.coolstuffmod.utils;
 
 public class Reference {
 	
-	public static final String MODID = "coolstuffmod";
+	public static final String MODID = "csm";
     public static final String NAME = "Cool stuff mod";
     public static final String VERSION = "0.1";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
