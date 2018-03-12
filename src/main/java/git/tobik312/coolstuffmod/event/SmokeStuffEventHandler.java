91b27136f68b47710e67a2663e795f04a34bb50f
tobik312 <tobik-10@tlen.pl>
2018-03-12 20:20:47 +0100

    update

--- src/main/java/git/tobik312/coolstuffmod/event/SmokeStuffEventHandler.java
+++ src/main/java/git/tobik312/coolstuffmod/event/SmokeStuffEventHandler.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import git.tobik312.coolstuffmod.items.ItemCigarette;
+import jline.internal.Log;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -18,7 +20,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
-public class SmokeStuffEventHandler {
+public class SmokeStuffEventHandler{
 	
 	public static float RENDER_SCALE = 0.0625F;
 	
@@ -39,21 +41,31 @@ public class SmokeStuffEventHandler {
     	EntityPlayer player = event.getEntityPlayer();
     	Item itemInHand = player.getHeldItem(EnumHand.MAIN_HAND).getItem();
     	if(itemInHand instanceof ItemCigarette && ((ItemCigarette) itemInHand).inUse){
+    		//BindTexture
     		ResourceLocation texture = event.getRenderer().getEntityTexture((AbstractClientPlayer) player);
     		event.getRenderer().bindTexture(texture);
-    		event.getRenderer().getMainModel().bipedRightArm.isHidden=false;
+    		//ChangeDisplay
+    		event.getRenderer().getMainModel().bipedRightArm.isHidden = false;
+    		//ChangeRotationPoints
     		event.getRenderer().getMainModel().bipedRightArm.rotationPointZ = -MathHelper.sin((float) Math.toRadians(player.renderYawOffset)) * 5.0F;
     		event.getRenderer().getMainModel().bipedRightArm.rotationPointY = 20;
     		event.getRenderer().getMainModel().bipedRightArm.rotationPointX = -MathHelper.cos((float) Math.toRadians(player.renderYawOffset)) * 5.0F;
+    		//
     		//Math.toRadians(player.rotationYawHead) - dependent
     		event.getRenderer().getMainModel().bipedRightArm.rotateAngleX = 1.22f;
     		event.getRenderer().getMainModel().bipedRightArm.rotateAngleY = (float) -Math.toRadians(player.renderYawOffset);
     		event.getRenderer().getMainModel().bipedRightArm.rotateAngleZ = -0.52f;
-    		//
+    		//Render
     		event.getRenderer().getMainModel().bipedRightArm.renderWithRotation(RENDER_SCALE);
     		event.getRenderer().getMainModel().bipedRightArm.rotationPointY = 2;
     	}
     }
+	
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public static void renderHandEvent(RenderHandEvent event){
+		
+	}
 
 	private static Map<EntityPlayer,Integer> playerTickCount = new HashMap<EntityPlayer,Integer>();
 	@SubscribeEvent
@@ -64,10 +76,10 @@ public class SmokeStuffEventHandler {
     	if(itemInHand instanceof ItemCigarette && ((ItemCigarette) itemInHand).inUse){
     		int actualPlayerTicks = playerTickCount.get(player);
     		playerTickCount.replace(player, ++actualPlayerTicks);
-    		if(actualPlayerTicks==80){
+    		/*if(actualPlayerTicks==80){
     			((ItemCigarette) itemInHand).inUse = false;
     			playerTickCount.replace(player,0);
-    		}
+    		}*/
     	}
 	}
 	
--- src/main/java/git/tobik312/coolstuffmod/items/ItemCigarette.java
+++ src/main/java/git/tobik312/coolstuffmod/items/ItemCigarette.java
@@ -1,14 +1,17 @@
 package git.tobik312.coolstuffmod.items;
 
-import org.lwjgl.opengl.GL11;
 
-import net.minecraft.client.renderer.ItemRenderer;
+import git.tobik312.coolstuffmod.CoolStuffMod;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 
 public class ItemCigarette extends ItemBuilder{
 	
@@ -17,16 +20,13 @@ public class ItemCigarette extends ItemBuilder{
 	public ItemCigarette() {
 		super("cigarette", 20);
 	}
-	
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn){
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand){
 		if(inUse) return new ActionResult<ItemStack>(EnumActionResult.FAIL,new ItemStack(this));
 		inUse = true;
+		player.setActiveHand(hand);
 		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS,new ItemStack(this));
-	}
-	
-	public void renderItem(ItemRenderer type, ItemStack stack, Object... data){
-		GL11.glTranslatef(300.0F, 7.0F, -1.5F);
-		GL11.glRotatef(30, 1, 1, 1);
 	}
 
 }
